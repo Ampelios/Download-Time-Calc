5c3d8ba77af7c5e4bc4158120c14701c151dbf28
Iqbal Razali <Iqbalrazali98@hotmail.com>
2023-05-22 17:39:58 +0800

    User Selection Works w CB

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,8 +20,15 @@ namespace Download_Time_Calc
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        double SpdMulti = 0;
+        String SpdUnit= "";
+        double SizeMulti = 0;
+        String SizeUnit = "";
+        double calc = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,126 +36,142 @@ namespace Download_Time_Calc
 
         private void LBSpeed_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int speed = LBSpeed.SelectedIndex;
-            int speedU = LBSpeedUnit.SelectedIndex;
-            String strout = "";
-            TBoxDeb.Text = "";
+            int speedIndex = LBSpeed.SelectedIndex;
+            int speedUIndex = LBSpeedUnit.SelectedIndex;
 
-            switch (speed)
+            switch (speedIndex)
             {
                 case 0:
                     {
-                        //TBoxDeb.Text = "Tera ";
-                        strout = "Speed Tera ";
-                        break;
+                        SpdMulti = Math.Pow(10,12);
+                        break;                        
                     }
                 case 1:
                     {
-                        //TBoxDeb.Text = "Giga ";
-                        strout = "Speed Giga ";
+                        SpdMulti = Math.Pow(10, 9);
                         break;
                     }
                 case 2:
                     {
-                        //TBoxDeb.Text = "Mega ";
-                        strout = "Speed Mega ";
+                        SpdMulti = Math.Pow(10, 6);
                         break;
                     }
                 case 3:
                     {
-                        //TBoxDeb.Text = "Kilo ";
-                        strout = "Speed Kilo ";
+                        SpdMulti = Math.Pow(10, 3);
                         break;
                     }
             }
 
-            switch (speedU)
+            switch (speedUIndex)
             {
                 case 0:
                     {
-                        strout = strout + "Bit";
+                        SpdUnit = " Bit";
                         break;
                     }
                 case 1:
                     {
-                        strout = strout + "Byte";
+                        SpdUnit = " Byte";
                         break;
                     }
                 
             }
-            TBoxDeb.Text = strout;
+            kira();
 
 
         }
 
         private void LBSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int size = LBSize.SelectedIndex;
-            int sizeU = LBSizeUnit.SelectedIndex;
-            String strout = "";
-            TBoxDeb.Text = "";
+            int sizeIndex = LBSize.SelectedIndex;
+            int sizeUIndex = LBSizeUnit.SelectedIndex;
 
-            switch (size)
+            switch (sizeIndex)
             {
                 case 0:
                     {
-                        //TBoxDeb.Text = "Tera ";
-                        strout = "Size Tera ";
+                        SizeMulti = Math.Pow(10, 12);
                         break;
                     }
                 case 1:
                     {
-                        //TBoxDeb.Text = "Giga ";
-                        strout = "Size Giga ";
+                        SizeMulti = Math.Pow(10, 9);
                         break;
                     }
                 case 2:
                     {
-                        //TBoxDeb.Text = "Mega ";
-                        strout = "Size Mega ";
+                        SizeMulti = Math.Pow(10, 6);
                         break;
                     }
                 case 3:
                     {
-                        //TBoxDeb.Text = "Kilo ";
-                        strout = "Size Kilo ";
+                        SizeMulti = Math.Pow(10, 3);
                         break;
                     }
             }
 
-            switch (sizeU)
+            switch (sizeUIndex)
             {
                 case 0:
                     {
-                        strout = strout + "Bit ";
+                        SizeUnit = " Bit";
                         break;
                     }
                 case 1:
                     {
-                        strout = strout + "Byte ";
+                        SizeUnit = " Byte";
                         break;
                     }
 
             }
-            TBoxDeb.Text = strout;
-
-
+            kira();
         }
 
         private void ButtClear_Click(object sender, RoutedEventArgs e)
         {
 
-            TBoxSpeed.Text = "";
-            TBoxSize.Text = "";
-            TBoxDeb.Text = "";
+            TBoxSpeed.Clear();
+            TBoxSize.Clear();
+            TBoxDeb.Clear();
             LBSpeed.UnselectAll();
             LBSpeedUnit.UnselectAll();
             LBSize.UnselectAll();
             LBSizeUnit.UnselectAll();
-            TBxDay.Text = "";
-            TBxHr.Text = "";
-            TBxMin.Text = "";
-            TBxSec.Text = "";
+            TBxDay.Clear();
+            TBxHr.Clear();
+            TBxMin.Clear();
+            TBxSec.Clear();
+        }
+
+        void runboi()
+        {
+
+        }
+        private void RunCalc (object sender, TextChangedEventArgs e)
+        { 
+            kira();   
+        }
+
+        private void NumbOnly (object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        void kira()
+        {
+            if (TBoxSpeed.Text != "" && TBoxSize.Text != "" && SpdMulti != 0 && SizeMulti != 0)
+            {
+                calc = (double.Parse(TBoxSize.Text) * SizeMulti) / (double.Parse(TBoxSpeed.Text) * SpdMulti);
+                TBoxDeb.Text = calc.ToString();
+            }
+            else
+            {
+                TBoxDeb.Text = "";
+            }
+            
         }
     }
+
 }
